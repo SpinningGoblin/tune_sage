6db7e9c15ed865a963b5e791c8c4c6570b5637b2
Derrick Plotsky <derrickplotsky@gmail.com>
2022-10-07 21:21:29 -0700

    Flesh out artist querying
    Now we can actually query for artists by a bunch of different params,
    and use the score to try to find the best fit.

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,7 +1,13 @@
 mod artists;
 mod config;
+mod general_options;
+mod query_operator;
 mod remote;
 
-pub use artists::{ArtistApi, ArtistQuery, ArtistSearch, ArtistSearchBuilder};
+pub use artists::{
+    ArtistApi, ArtistGenderQuery, ArtistQuery, ArtistSearch, ArtistSearchBuilder,
+    ArtistSearchBuilderError,
+};
 pub use config::Config;
+pub use general_options::{GeneralOptions, GeneralOptionsBuilder, GeneralOptionsBuilderError};
 pub use remote::{HttpRemote, Remote};
