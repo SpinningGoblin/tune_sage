92a6cea5f65bb986c4749ca906e1c240aaec04bf
Derrick Plotsky <derrickplotsky@gmail.com>
2022-10-07 21:23:42 -0700

    Add some more to our main example

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
-use tune_sage::api::{ArtistApi, ArtistQuery, Config, HttpRemote};
+use tune_sage::api::{
+    ArtistApi, ArtistQuery, ArtistSearchBuilder, Config, HttpRemote,
+};
 
 #[tokio::main]
 pub async fn main() {
     let http_remote = HttpRemote;
-    let _artist_id = "ce8a3258-1863-469c-a832-37d22e7af624";
+    let artist_id = "ce8a3258-1863-469c-a832-37d22e7af624";
     let config = Config {
         base_url: "https://musicbrainz.org/ws/2".to_string(),
         user_agent: "TuneSage <https://github.com/derrickp/musicz>".to_string(),
@@ -14,9 +16,22 @@ pub async fn main() {
         remote: Box::new(http_remote),
     };
 
+    println!("{}", remote.by_id(artist_id).await.unwrap().name);
+
+    let search = ArtistSearchBuilder::new()
+        .tag("death metal")
+        .artist("death")
+        .build()
+        .unwrap();
+
     let artist_list = remote
-        .query(&ArtistQuery::Term("Slipknot".to_string()))
+        .query(&ArtistQuery::Search(Box::new(search)), None)
         .await
         .unwrap();
-    println!("{:?}", artist_list);
+
+    for artist in artist_list.artists.iter() {
+        println!("{}", artist.name);
+    }
+
+    println!("{:?} {:?}", artist_list.count, artist_list.artists.len());
 }
